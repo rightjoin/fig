25778ffad1cee2ae43a1f7211be364607e6aa370
Harsh K <hkundra@gmail.com>
2018-01-21 19:11:20 +0530

    base code and test cases

---
+++ fig.go
@@ -0,0 +1,223 @@
+package fig
+
+import (
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"strings"
+
+	"github.com/serenize/snaker"
+
+	"github.com/jacobstr/confer"
+)
+
+// TODO:
+// - allow command line arguments to be passed to override configurations
+// - eg. --APP_PORT=1234, should be bubbled to the right place
+var configuration *confer.Config
+
+// FileOrder contains the name, path and order in which the configuration
+// files will be read. The first file will have highest priority
+var FileOrder = []string{
+	// highest
+	"./config/production.yaml",
+	"./config/production.json",
+	"production.yaml",
+	"production.json",
+
+	// medium
+	"./config/dev.yaml",
+	"./config/dev.json",
+	"dev.yaml",
+	"dev.json",
+
+	// lowest
+	"./config/config.yaml",
+	"./config/config.json",
+	"config.yaml",
+	"config.json",
+}
+
+// ShowFiles if set to true, will output the files to StdOut
+// which were read to build the overall configuration
+var ShowFiles = false
+
+// jumpstart loads the configuration
+func jumpstart() {
+
+	var filesOk = []string{}
+
+	// loop in reverse order (lowest priority first)
+	for i := len(FileOrder) - 1; i >= 0; i-- {
+		tmp := confer.NewConfig()
+		err := tmp.ReadPaths(FileOrder[i])
+		if err == nil {
+			// file found (path is good)
+			abs, _ := filepath.Abs(FileOrder[i])
+			filesOk = append(filesOk, abs)
+		}
+	}
+
+	configuration = confer.NewConfig()
+	configuration.ReadPaths(filesOk...)
+	if ShowFiles {
+		fmt.Println("fig configuration loaded:", strings.Join(filesOk, " → "))
+	}
+}
+
+// exists checks if the given key is present.
+// It also loads configuration (if missing)
+func exists(keys ...string) bool {
+	if configuration == nil {
+		jumpstart()
+	}
+
+	key := strings.Join(keys, ".")
+	return configuration.IsSet(key)
+}
+
+// mustExist panics if the given configuration key is missing
+func mustExist(key string) {
+	if !exists(key) {
+		panic("configuration key missing:" + key)
+	}
+}
+
+// IntOr returns the int value at the given key.
+// If key is missing it returns defaultVal
+func IntOr(defaultVal int, keys ...string) int {
+	key := strings.Join(keys, ".")
+	if !exists(key) {
+		return defaultVal
+	}
+
+	return configuration.GetInt(key)
+}
+
+// Int returns the int value at the given key.
+// Panics if the key is missing.
+func Int(keys ...string) int {
+	key := strings.Join(keys, ".")
+	mustExist(key)
+	return configuration.GetInt(key)
+}
+
+// FloatOr returns the float64 value at the given key.
+// If key is missing it returns defaultVal
+func FloatOr(defaultVal float64, keys ...string) float64 {
+	key := strings.Join(keys, ".")
+	if !exists(key) {
+		return defaultVal
+	}
+
+	return configuration.GetFloat64(key)
+}
+
+// Float returns the float value at the given key.
+// Panics if the key is missing.
+func Float(keys ...string) float64 {
+	key := strings.Join(keys, ".")
+	mustExist(key)
+	return configuration.GetFloat64(key)
+}
+
+// StringOr returns the string value at the given key.
+// If key is missing it returns defaultVal
+func StringOr(defaultVal string, keys ...string) string {
+	key := strings.Join(keys, ".")
+	if !exists(key) {
+		return defaultVal
+	}
+
+	return configuration.GetString(key)
+}
+
+// String returns the string value at the given key.
+// Panics if the key is missing.
+func String(keys ...string) string {
+	key := strings.Join(keys, ".")
+	mustExist(key)
+	return configuration.GetString(key)
+}
+
+// BoolOr returns the bool value at the given key.
+// If key is missing it returns defaultVal
+func BoolOr(defaultVal bool, keys ...string) bool {
+	key := strings.Join(keys, ".")
+	if !exists(key) {
+		return defaultVal
+	}
+
+	return configuration.GetBool(key)
+}
+
+// Bool returns the bool value at the given key.
+// Panics if the key is missing.
+func Bool(keys ...string) bool {
+	key := strings.Join(keys, ".")
+	mustExist(key)
+	return configuration.GetBool(key)
+}
+
+// Map returns a map a the given key.
+// Panics if the key is missing.
+func Map(keys ...string) map[string]interface{} {
+	key := strings.Join(keys, ".")
+	mustExist(key)
+	return configuration.GetStringMap(key)
+}
+
+// Struct is used to parse and load simple structures. Most common use is
+// reading connection strings.
+// Note that it does not work for nested structs or arrays
+func Struct(addr interface{}, keys ...string) {
+
+	container := strings.Join(keys, ".")
+
+	// addr's underlying type should be a struct address
+	utype := reflect.TypeOf(addr)
+	if utype.Kind() != reflect.Ptr || utype.Elem().Kind() != reflect.Struct {
+		panic("Struct() method expects address of a struct")
+	}
+
+	rtype := reflect.TypeOf(addr).Elem()
+	rvalue := reflect.ValueOf(addr).Elem()
+
+	// loop: all the fields of this type
+	for i := 0; i < rtype.NumField(); i++ {
+		ftype := rtype.Field(i)
+		fname := ftype.Name // field name
+
+		// lookup the key in a number of formats
+		// eg, a field name called NumItems could be read as:
+		//  - NumItems  [exact]
+		//  - numitems  [lowercase]
+		//  - num_items [snakecase]
+		//  - num-items [urlcase]
+		lookup := []string{fname, strings.ToLower(fname), snaker.CamelToSnake(fname), strings.Replace(snaker.CamelToSnake(fname), "_", "-", -1)}
+		found := false
+		for _, amatch := range lookup {
+			//fmt.Println("checking:", container, amatch)
+			if exists(container, amatch) {
+				found = true
+				switch fmt.Sprintf("%s", ftype.Type) {
+				case "string":
+					val := String(container, amatch)
+					rvalue.Field(i).SetString(val)
+				case "int":
+					val := Int(container, amatch)
+					rvalue.Field(i).SetInt(int64(val))
+				default:
+					panic("Can not read any value other than string|int")
+				}
+			}
+
+		}
+		if !found {
+			// if the field was not an optional one, then throw error
+			if !strings.Contains(ftype.Tag.Get("fig"), "optional") {
+				panic("Can not find value for: " + container + ".(" + strings.Join(lookup, "|") + ")")
+			}
+		}
+	}
+}
---
+++ fig_test.go
@@ -0,0 +1,103 @@
+package fig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBool(t *testing.T) {
+	configuration = nil
+
+	assert.Equal(t, true, Bool("boolean"))
+
+	assert.Equal(t, true, BoolOr(true, "something"))
+	assert.Equal(t, true, BoolOr(false, "boolean"))
+}
+
+func TestInt(t *testing.T) {
+	configuration = nil
+
+	assert.Equal(t, 123, Int("integer"))
+
+	assert.Equal(t, 456, IntOr(456, "something"))
+	assert.Equal(t, 123, IntOr(456, "integer"))
+}
+
+func TestString(t *testing.T) {
+	configuration = nil
+
+	assert.Equal(t, "hello", String("string"))
+
+	assert.Equal(t, "world", StringOr("world", "something"))
+	assert.Equal(t, "hello", StringOr("world", "string"))
+}
+
+func TestFloat(t *testing.T) {
+	configuration = nil
+
+	assert.Equal(t, 12.34, Float("floating-point"))
+
+	assert.Equal(t, 34.12, FloatOr(34.12, "something"))
+	assert.Equal(t, 12.34, FloatOr(34.12, "floating-point"))
+}
+
+func TestOverriding(t *testing.T) {
+
+	configuration = nil
+	assert.Equal(t, "json", String("tooverride"))
+
+	// now lets change FileOrder
+	oldOrder := FileOrder
+	configuration = nil
+	FileOrder = []string{"./config/config.yaml", "./config/dev.json"}
+	assert.Equal(t, "yaml", String("tooverride"))
+	FileOrder = oldOrder
+}
+
+func TestPanic(t *testing.T) {
+	configuration = nil
+	assert.Panics(t, func() {
+		String("something")
+	})
+}
+
+func TestStruct(t *testing.T) {
+	configuration = nil
+
+	type Parent struct {
+		IntValue      int
+		StringValue   string
+		OptionalValue string `fig:"optional"`
+	}
+
+	var exact, lower, snake, url, optional Parent
+
+	Struct(&exact, "struct", "exact")
+	assert.Equal(t, 123, exact.IntValue)
+	assert.Equal(t, "abc", exact.StringValue)
+
+	Struct(&lower, "struct", "lower")
+	assert.Equal(t, 123, lower.IntValue)
+	assert.Equal(t, "abc", lower.StringValue)
+
+	Struct(&snake, "struct", "snake")
+	assert.Equal(t, 123, snake.IntValue)
+	assert.Equal(t, "abc", snake.StringValue)
+
+	Struct(&url, "struct", "url")
+	assert.Equal(t, 123, url.IntValue)
+	assert.Equal(t, "abc", url.StringValue)
+
+	Struct(&optional, "struct", "optional")
+	assert.Equal(t, 123, optional.IntValue)
+	assert.Equal(t, "abc", optional.StringValue)
+	assert.Equal(t, "def", optional.OptionalValue)
+}
+
+func TestMap(t *testing.T) {
+	configuration = nil
+	data := Map("map")
+	assert.Equal(t, 1234, data["field-a"])
+	assert.Equal(t, "abcd", data["field-b"])
+}
